6621bf0f5e9b29485a97906eb588f2906cf355e7
Geraldo Fada <geraldofada.neto@gmail.com>
2019-06-16 15:03:38 -0300

    enemy shooting, lives, score and gameover

--- space-invaders/actors.py
+++ space-invaders/actors.py
@@ -1,3 +1,4 @@
+from random import randint
 from PPlay.sprite import Sprite
 from PPlay.animation import Animation
 from PPlay.keyboard import Keyboard
@@ -13,6 +14,7 @@ class SpaceShip(object):
         self.alien = alien
 
         self.sprite = Sprite("./assets/actors/spaceship.png")
+        self.player_was_shot = False
 
         self.speed = globals.SPACESHIP_VEL
         self.reload_cron = 0
@@ -38,6 +40,11 @@ class SpaceShip(object):
                 )
                 self.reload_cron = self.reload_time
 
+        for bullet in self.bullet.bullets_alien:
+            if self.sprite.collided(bullet):
+                self.bullet.bullets_alien.remove(bullet)
+                self.player_was_shot = True
+
         for ali in self.alien.aliens:
             if self.sprite.collided(ali):
                 globals.GAME_STATE = 0
@@ -55,49 +62,73 @@ class SpaceShip(object):
 
 
 class Bullet(object):
-    def __init__(self, window, aliens):
+    def __init__(self, window):
         self.window = window
-        self.aliens = aliens
-
-        self.killed_alien = False
 
         self.bullets = []
+        self.bullets_alien = []
         self.speed = globals.BULLET_VEL
 
-    def spawn(self, initial_x, initial_y):
+    def spawn(self, initial_x, initial_y, was_alien=False):
         bullet = Sprite("./assets/actors/bullet.png")
         bullet.set_position(initial_x - bullet.width / 2, initial_y - bullet.height)
-        self.bullets.append(bullet)
+        if was_alien == False:
+            self.bullets.append(bullet)
+        else:
+            self.bullets_alien.append(bullet)
 
     def update(self):
-        for bullet in self.bullets:
-            for alien in self.aliens:
-                if bullet.collided(alien):
-                    self.bullets.remove(bullet)
-                    self.aliens.remove(alien)
-                    self.killed_alien = True
-
         if len(self.bullets) > 0:
             for bullet in self.bullets:
                 if bullet.y + bullet.height < 0:
                     self.bullets.remove(bullet)
 
-        for i in range(len(self.bullets)):
-            self.bullets[i].y -= self.speed * self.window.delta_time()
-            self.bullets[i].draw()
+        for bullet in self.bullets:
+            bullet.y -= self.speed * self.window.delta_time()
+            bullet.draw()
+
+        if len(self.bullets_alien) > 0:
+            for bullet in self.bullets_alien:
+                if bullet.y + bullet.height < 0:
+                    self.bullets_alien.remove(bullet)
+
+        for bullet in self.bullets_alien:
+            bullet.y += self.speed * self.window.delta_time()
+            bullet.draw()
 
 
 class Aliens(object):
-    def __init__(self, window, tuple_x_y, multiplier=1):
+    def __init__(self, window, tuple_x_y, bullet, multiplier=1):
         self.window = window
         self.tuple_x_y = tuple_x_y
+        self.bullet = bullet
+
         self.speed = globals.ALIEN_VEL * multiplier
+        self.reload_time = globals.ALIEN_RELOAD_TIME
+        self.reload_cron = 0
+
+        self.alien_killed = False
+        self.alien_shooting = False
 
         self.aliens = []
         self.__setup()
 
     def update(self):
+        if self.reload_cron <= 0 and self.alien_shooting == False:
+            index_alien_shooting = randint(0, len(self.aliens) - 1)
+            alien_shooting_x = self.aliens[index_alien_shooting].x + self.aliens[index_alien_shooting].width/2
+            alien_shooting_y = self.aliens[index_alien_shooting].y + self.aliens[index_alien_shooting].height
+
+            self.bullet.spawn(alien_shooting_x, alien_shooting_y, True)
+            self.reload_cron = self.reload_time
+
         for alien in self.aliens:
+            for bullet in self.bullet.bullets:
+                if bullet.collided(alien):
+                        self.bullet.bullets.remove(bullet)
+                        self.aliens.remove(alien)
+                        self.alien_killed = True
+
             alien.x += self.speed * self.window.delta_time()
             alien.update()
             alien.draw()
@@ -113,6 +144,8 @@ class Aliens(object):
                     alien.set_position(alien.x - 1, alien.y + 20)
                 self.speed *= -1
                 break
+
+        self.reload_cron -= 1
 
     def __setup(self):
         for i in range(self.tuple_x_y[1]):
--- space-invaders/play.py
+++ space-invaders/play.py
@@ -13,6 +13,7 @@ class Play(object):
 
         self.score = 0
         self.level = 1
+        self.lives = 2
         self.time = 0
         self.multiplier = 0
         self.difficulty = ""
@@ -26,8 +27,8 @@ class Play(object):
 
         self.update_difficulty()
 
-        self.alien = Aliens(self.window, (5, 2), self.multiplier)
-        self.bullet = Bullet(self.window, self.alien.aliens)
+        self.bullet = Bullet(self.window)
+        self.alien = Aliens(self.window, (5, 2), self.bullet, self.multiplier)
         self.spaceship = SpaceShip(
             self.window, self.bullet, self.alien, self.multiplier
         )
@@ -45,36 +46,47 @@ class Play(object):
         if len(self.alien.aliens) <= 0:
             self.time = 0
             self.level += 1
+
+            self.bullet.__init__(self.window)
+
             if self.level == 2:
-                self.alien.__init__(self.window, (5, 3), self.multiplier)
+                self.alien.__init__(self.window, (5, 3), self.bullet, self.multiplier)
             elif self.level == 3:
-                self.alien.__init__(self.window, (6, 4), self.multiplier)
+                self.alien.__init__(self.window, (6, 4), self.bullet, self.multiplier)
                 self.multiplier += 0.5
             elif self.level == 4:
-                self.alien.__init__(self.window, (8, 5), self.multiplier)
+                self.alien.__init__(self.window, (8, 5), self.bullet, self.multiplier)
             elif self.level == 5:
-                self.alien.__init__(self.window, (9, 5), self.multiplier)
+                self.alien.__init__(self.window, (9, 5), self.bullet, self.multiplier)
                 self.multiplier += 0.5
             elif self.level == 6:
-                self.alien.__init__(self.window, (11, 5), self.multiplier)
+                self.alien.__init__(self.window, (11, 5), self.bullet, self.multiplier)
             elif self.level == 7:
-                self.alien.__init__(self.window, (13, 6), self.multiplier)
+                self.alien.__init__(self.window, (13, 6), self.bullet, self.multiplier)
                 self.multiplier += 0.5
             elif 15 > self.level > 7:
-                self.alien.__init__(self.window, (15, 6), self.multiplier)
+                self.alien.__init__(self.window, (15, 6), self.bullet, self.multiplier)
                 self.multiplier += 0.1
             else:
-                self.alien.__init__(self.window, (15, 6), self.multiplier)
+                self.alien.__init__(self.window, (15, 6), self.bullet, self.multiplier)
 
             self.update_difficulty()
-            self.bullet.__init__(self.window, self.alien.aliens)
             self.spaceship.__init__(
                 self.window, self.bullet, self.alien, self.multiplier
             )
 
-        if self.bullet.killed_alien == True:
+        if self.alien.alien_killed == True:
             self.score += self.get_score(self.time) * self.multiplier
-            self.bullet.killed_alien = False
+            self.alien.alien_killed = False
+        
+        if self.spaceship.player_was_shot == True:
+            self.spaceship.__init__(
+                self.window, self.bullet, self.alien, self.multiplier
+            )
+            self.lives -= 1
+            if self.lives < 0:
+                globals.GAME_STATE = 0
+                globals.PLAY_INIT = True
 
         self.window.draw_text_font(
             "SCORE: {0:.2f}".format(self.score),
@@ -87,6 +99,13 @@ class Play(object):
             "DIFFICULTY: {}".format(self.difficulty),
             "./assets/fonts/pixelmix.ttf",
             globals.SCREEN_BORDER + 200,
+            self.window.height - globals.SCREEN_BORDER,
+        )
+
+        self.window.draw_text_font(
+            "LIVES: {}".format(self.lives),
+            "./assets/fonts/pixelmix.ttf",
+            globals.SCREEN_BORDER + 700,
             self.window.height - globals.SCREEN_BORDER,
         )
 
